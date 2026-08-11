d75baf15827ed596cd1d504a5edaa3356f1b2038
Xiaomei Mi <135629372+xm2325@users.noreply.github.com>
2026-08-11 17:38:33 +0100

    Add cross-cohort regulatory overlap

--- scripts/compare_regulatory_receipts.py
+++ scripts/compare_regulatory_receipts.py
@@ -1,3 +1,6 @@
+** WARNING: connection is not using a post-quantum key exchange algorithm.
+** This session may be vulnerable to "store now, decrypt later" attacks.
+** The server may need to be upgraded. See https://openssh.com/pq.html
 #!/usr/bin/env python3
 """Compare response-blind regulatory receipts across cohorts."""
 
